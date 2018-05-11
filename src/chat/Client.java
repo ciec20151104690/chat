031de588bec9bdfc59a0b558819d066fa7dcdb09
Cco <1365084312@qq.com>
2018-05-11 16:52:50 +0800

    添加了消息窗体

--- src/chat/Client.java
+++ src/chat/Client.java
@@ -9,12 +9,15 @@ import javax.swing.JTextField;
 import javax.swing.JButton;
 import java.awt.Component;
 import javax.swing.Box;
+import javax.swing.JPasswordField;
+import javax.swing.JTextPane;
 
 public class Client {
 
 	private JFrame frame;
-	private JTextField textField;
-	private JTextField textField_1;
+	private JTextField textFieldUserName;
+	private JPasswordField passwordFieldpwd;
+	private JTextField textFieldMsgToSend;
 
 	/**
 	 * Launch the application.
@@ -52,19 +55,14 @@ public class Client {
 		lblNewLabel.setBounds(0, 0, 65, 24);
 		frame.getContentPane().add(lblNewLabel);
 		
-		textField = new JTextField();
-		textField.setBounds(56, 0, 114, 24);
-		frame.getContentPane().add(textField);
-		textField.setColumns(10);
+		textFieldUserName = new JTextField();
+		textFieldUserName.setBounds(56, 0, 114, 24);
+		frame.getContentPane().add(textFieldUserName);
+		textFieldUserName.setColumns(10);
 		
 		JLabel label = new JLabel("\u5BC6\u7801\uFF1A");
 		label.setBounds(184, 3, 45, 18);
 		frame.getContentPane().add(label);
-		
-		textField_1 = new JTextField();
-		textField_1.setBounds(225, 0, 100, 24);
-		frame.getContentPane().add(textField_1);
-		textField_1.setColumns(10);
 		
 		JButton btnNewButton = new JButton("\u767B\u9646");
 		btnNewButton.setBounds(343, -1, 80, 27);
@@ -73,5 +71,23 @@ public class Client {
 		JButton btnNewButton_1 = new JButton("\u6CE8\u518C");
 		btnNewButton_1.setBounds(437, 0, 80, 25);
 		frame.getContentPane().add(btnNewButton_1);
+		
+		passwordFieldpwd = new JPasswordField();
+		passwordFieldpwd.setBounds(225, 0, 104, 24);
+		frame.getContentPane().add(passwordFieldpwd);
+		
+		JTextPane textPaneMsgRecord = new JTextPane();
+		textPaneMsgRecord.setText("\u6D88\u606F\u8BB0\u5F55");
+		textPaneMsgRecord.setBounds(14, 49, 424, 202);
+		frame.getContentPane().add(textPaneMsgRecord);
+		
+		textFieldMsgToSend = new JTextField();
+		textFieldMsgToSend.setBounds(14, 279, 313, 24);
+		frame.getContentPane().add(textFieldMsgToSend);
+		textFieldMsgToSend.setColumns(10);
+		
+		JButton btnNewButton_2 = new JButton("\u53D1\u9001\u6D88\u606F");
+		btnNewButton_2.setBounds(345, 278, 93, 27);
+		frame.getContentPane().add(btnNewButton_2);
 	}
 }
