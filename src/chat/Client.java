ce80e24997da6b766b932d877267c9cdc1227412
Cco <1365084312@qq.com>
2018-05-11 09:50:51 +0800

    Client界面
    简单加了些界面

--- src/chat/Client.java
+++ src/chat/Client.java
@@ -3,10 +3,18 @@ package chat;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import java.awt.BorderLayout;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.Component;
+import javax.swing.Box;
 
 public class Client {
 
 	private JFrame frame;
+	private JTextField textField;
+	private JTextField textField_1;
 
 	/**
 	 * Launch the application.
@@ -38,6 +46,32 @@ public class Client {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 563, 363);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("\u7528\u6237\u540D\uFF1A");
+		lblNewLabel.setBounds(0, 0, 65, 24);
+		frame.getContentPane().add(lblNewLabel);
+		
+		textField = new JTextField();
+		textField.setBounds(56, 0, 114, 24);
+		frame.getContentPane().add(textField);
+		textField.setColumns(10);
+		
+		JLabel label = new JLabel("\u5BC6\u7801\uFF1A");
+		label.setBounds(184, 3, 45, 18);
+		frame.getContentPane().add(label);
+		
+		textField_1 = new JTextField();
+		textField_1.setBounds(225, 0, 100, 24);
+		frame.getContentPane().add(textField_1);
+		textField_1.setColumns(10);
+		
+		JButton btnNewButton = new JButton("\u767B\u9646");
+		btnNewButton.setBounds(343, -1, 80, 27);
+		frame.getContentPane().add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("\u6CE8\u518C");
+		btnNewButton_1.setBounds(437, 0, 80, 25);
+		frame.getContentPane().add(btnNewButton_1);
 	}
-
 }
